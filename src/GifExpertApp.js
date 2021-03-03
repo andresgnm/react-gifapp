b7218ac49a7a4cb4b0a89da0a47d8fe680dac476
andresgnm <andresgnm13@gmail.com>
2021-03-03 15:44:57 -0600

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
+
+
+const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState( ['Rick and Morty'])
+    
+    //const handleAdd = () => {
+       // //setCategories(['Flash', ..categories]),
+        //setCategories([...categories, 'Flash']);
+    //}
+    return (
+        <>
+           <h2>GifExpertApp</h2>
+           <AddCategory setCategories = {setCategories}/>
+           <hr></hr>
+           <ol>
+               { 
+                    categories.map( category => (
+                        <GifGrid 
+                        key = {category}
+                        category = {category} 
+                        />
+                    )) 
+                }
+           </ol>
+        </>
+    )
+}
+
+
+export default GifExpertApp
